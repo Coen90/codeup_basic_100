08b0f5a05e6bbaca9acd459b7e6f1315b8b6d93d
coen90 <bht9011@gmail.com>
2023-01-09 09:08:38 +0900

    modify some code

--- codeup/java/Codeup1010.java
+++ codeup/java/Codeup1010.java
@@ -4,6 +4,7 @@ public class Codeup1010 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
+    sc.close();
     System.out.println(a);
   }
 }
--- codeup/java/Codeup1011.java
+++ codeup/java/Codeup1011.java
@@ -4,6 +4,7 @@ public class Codeup1011 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     char c = sc.next().charAt(0);
+    sc.close();
     System.out.println(c);
   }
 }
