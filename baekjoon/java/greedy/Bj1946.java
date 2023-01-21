033c462e5da34e805fc497085b0fe7619d9f5a9c
Coen90 <81370558+Coen90@users.noreply.github.com>
2023-01-21 22:42:22 +0900

    Baekjoon greedy 1946 solved

--- baekjoon/java/greedy/Bj1946.java
+++ baekjoon/java/greedy/Bj1946.java
@@ -4,30 +4,35 @@ import java.io.*;
 import java.util.*;
 
 public class Bj1946 {
-  public static void main(String[] args) throws NumberFormatException, IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    int tc = Integer.parseInt(reader.readLine());
+  public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    while (tc-- > 0) {
-      int n = Integer.parseInt(reader.readLine());
-      int[] a = new int[n + 1];
-
-      for (int i = 0; i < n; i++) {
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        a[x] = y;
-      }
-
-      int cnt = 1; // x가 1일때는 무조건 가능하므로 1로 시작
-      int standard = a[1]; // 기준 값, 처음에는 x가 1일 때의 y값
-      for (int i = 2; i <= n; i++) {
-        if (standard > a[i]) { // 기준 값보다 a[i]의 y값이 작다면
-          cnt++; // 추가
-          standard = a[i]; // 기준 값 a[i]의 y값으로 변경
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            int interviewee = Integer.parseInt(br.readLine());
+            int[] arr = new int[interviewee + 1];
+            for (int j = 0; j < interviewee; j++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                arr[a] = b;
+            }
+            
+            int cutLine = arr[1];
+            int cnt = 1;
+            for (int j = 2; j <= interviewee; j++) {
+                if(arr[j] < cutLine) {
+                    cutLine = arr[j];
+                    cnt++;
+                } 
+            }
+            bw.append(cnt + "\n");
         }
-      }
-      System.out.println(cnt);
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
-  }
 }
