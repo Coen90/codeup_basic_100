161666bfd9566a420398d32584e1ef4c03aba1dd
coen90 <bht9011@gmail.com>
2023-02-01 08:58:57 +0900

    add solutions

---
+++ programmers/dfs/TargetNumber.java
@@ -0,0 +1,29 @@
+package dfs;
+
+class Solution {
+  int[] numbers;
+  int target;
+  int answer;
+
+  void dfs(int index, int sum) {
+    // 1. 탈출조건
+    if (index == numbers.length) {
+      if (sum == target)
+        answer++;
+      return;
+    }
+
+    // 2. 수행동작
+    dfs(index + 1, sum + numbers[index]);
+    dfs(index + 1, sum - numbers[index]);
+  }
+
+  public int solution(int[] numbers, int target) {
+    this.numbers = numbers;
+    this.target = target;
+
+    dfs(0, 0);
+
+    return answer;
+  }
+}
